1382bc969421580d84edc60e9feea8f33cf00024
Hymeck <noonimf@gmail.com>
2020-10-02 14:53:17 +0300

    not important

---
+++ app/src/main/java/by/hymeck/unitconverter/viewmodels/MassViewModel.java
@@ -0,0 +1,16 @@
+package by.hymeck.unitconverter.viewmodels;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+//import domain.mass.Mass;
+//import domain.mass.MassConverter;
+//
+//public class MassViewModel extends ViewModel
+//{
+//    private MutableLiveData<Mass> fromValue;
+//    private MutableLiveData<Mass> toValue;
+//    private static final MassConverter massConverter = new MassConverter();
+//
+//
+//}
